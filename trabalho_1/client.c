949c24f8da11fb01cf03fd379bc46f3d64610e6e
Fealps <felipeboccardi@hotmail.com>
2021-08-28 12:44:14 -0300

    Criacao do socket TCP
    Co-authored-by: Giovannabbottino <giovannabbottino@gmail.com>

--- trabalho_1/client.c
+++ trabalho_1/client.c
@@ -5,7 +5,30 @@
 #include <stdio.h> /* printf(), perror() */
 #include <stdlib.h> 
 #include "properties.h" /* informacoes para rodar */
+#include <netinet/in.h> /* struct sockaddr_in */
 
 int main(int argc, char *argv[]) {
+    int client;
+    struct sockaddr_in server_address, client_address; /* socket do servidor e cliente  */
 
+    /* Criacao do socket TCP */
+    if ((client = socket(AF_INET, SOCK_STREAM, 0)) == 0){ 
+        /*
+        * ARPA INTERNET PROTOCOLS -- IPv4
+        * SOCK_STREAM orientado a conexão com transmissão de fluxos de bytes, sequencial e bidirecional
+        * 0 - protocolo padrao para o tipo escolhido -- TCP
+        */
+        perror("CLIENT: Criação do socket falhou");
+        exit(0);
+    }
+
+    /* Preenchendo informacoes sobre o servidor */
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(SERVER_HOST); 
+    server_address.sin_port = htons(SERVER_PORT);
+
+    /* Preenchendo informacoes sobre o cliente */
+    client_address.sin_family = AF_INET;
+    client_address.sin_addr.s_addr = inet_addr(CLIENT_HOST); 
+    client_address.sin_port = htons(CLIENT_PORT);
 }
