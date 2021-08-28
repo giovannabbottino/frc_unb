26e4c69278193b3d2103068f70d701c7166dab1e
Fealps <felipeboccardi@hotmail.com>
2021-08-28 11:41:37 -0300

    Abertura do client
    Co-authored-by: Giovannabbottino <giovannabbottino@gmail.com>

---
+++ trabalho_1/client.c
@@ -0,0 +1,11 @@
+/*
+* Giovanna Borges Bottino 170011267
+* Felipe Boccardi Silva Agustini 180119818
+*/
+#include <stdio.h> /* printf(), perror() */
+#include <stdlib.h> 
+#include "properties.h" /* informacoes para rodar */
+
+int main(int argc, char *argv[]) {
+
+}
