104043ddd3c4ca30e4f71f5987e90a217fc1c1cf
giovannabbottino <giovannabbottino@gmail.com>
2021-08-29 16:35:19 -0300

    tentativa de recebimento de mensagem

--- trabalho_1/server.c
+++ trabalho_1/server.c
@@ -46,31 +46,33 @@ int main(int argc, char *argv[]){
 
     /*  numero maximo de conexões */
     listen(server, 5);
-
-    /* Aceita conexoes direta entre o servidor e cliente */
-    client_size = sizeof(client_address);
-    if ( client = accept(server, (struct sockaddr *) &client_address, &client_size) == 1 ){
-        perror("SERVER: Não pode dar accept na CLIENT_PORT");
-        exit(0);
-    }
-
-    /* Receber pdu do client  */
-    char pdu_size[10];
-    recv (client, pdu_size, 10, 0);
-    /* o tamanho da mensagem pdu*/
-    pdu = atoi(pdu_size); 
-    char message[pdu];
-
-    printf("SERVER: Recebido o PDU da CLIENT_PORT: %d\n", pdu);
-
-    printf("SERVER: Esperando por dados no IP: %s, porta TCP numero: %d\n", SERVER_HOST, SERVER_PORT);
-
+    
     while(1){
+        
+        /* Aceita conexoes direta entre o servidor e cliente */
+        client_size = sizeof(client_address);
+        if ( client = accept(server, (struct sockaddr *) &client_address, &client_size) == 1 ){
+            perror("SERVER: Não pode dar accept na CLIENT_PORT");
+            exit(0);
+        }
 
-        /* Receber fila de mensagens*/
+        /* Receber pdu do client  */
+        char pdu_size[10];
+        recv (client, pdu_size, 10, 0);
+        /* o tamanho da mensagem pdu*/
+        pdu = atoi(pdu_size); 
+        char message[pdu];
 
-        /* imprime a mensagem recebida na tela do usuario */
-        printf("SERVER: {TPC, IP_L: %s, Porta_L: %u", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port));
-        printf("SERVER: IP_R: %s, Porta_R: %u} => %s\n",inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port), message);
-    }   
+        printf("SERVER: Recebido o PDU da CLIENT_PORT: %d\n", pdu);
+
+        printf("SERVER: Esperando por dados no IP: %s, porta TCP numero: %d\n", SERVER_HOST, SERVER_PORT);
+
+        while(1){
+            recv(client, message, pdu, 0);
+            printf("SERVER: Recebida a mensagem: %s\n", message);
+            
+            if (!strcmp (message, "exit")) break;
+
+        }   
+    }
 }
