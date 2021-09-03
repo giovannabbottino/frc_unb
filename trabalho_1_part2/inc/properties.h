43dc0cfbc472b1b2e7cbf9c38667b20045ebb6cb
giovannabbottino <giovannabbottino@gmail.com>
2021-09-02 22:03:05 -0300

    Adiciona unistd ao properties
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- trabalho_1_part2/inc/properties.h
+++ trabalho_1_part2/inc/properties.h
@@ -3,11 +3,12 @@
 #include <stdlib.h> 
 #include <string.h>
 #include <netinet/in.h> /* struct sockaddr_in */
-#include <sys/socket.h> /* connect() send() */
+#include <sys/socket.h> /* connect() send() bind() recv() */
 #include <arpa/inet.h>  /* htons(), inet_addr() */
-#include <sys/types.h> /* AF_INET, SOCK_STREAM */
+#include <sys/types.h> /* AF_INET, SOCK_STREAM pid_t */
 #include <strings.h> /* bzero() */
 #include <ctype.h> /* isdigit*/
+#include <unistd.h>  /* fork() */
 
 #define HOST "127.0.0.1"
 #define PORT 5055
